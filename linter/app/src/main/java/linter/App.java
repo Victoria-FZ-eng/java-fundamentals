11d3c33184b65fa723cb0eab96c365a4ecbf2fdd
Victoria-FZ-eng <vzulof@gmail.com>
2021-07-28 15:19:36 +0300

    fix 3

--- linter/app/src/main/java/linter/App.java
+++ linter/app/src/main/java/linter/App.java
@@ -14,11 +14,13 @@ import java.util.HashSet;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello from lab03!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println(new App().getGreeting());
+        javaScriptLinter();
     }
 
     public static void javaScriptLinter(){
